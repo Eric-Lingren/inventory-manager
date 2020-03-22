ac374d37c7ccd257e5ffbe811f51b71692ca72b0
Eric Lingren <ericlingren@gmail.com>
2020-03-22 15:43:43 -0600

    styling home page

--- client/src/global/Home.js
+++ client/src/global/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import unorganizedImage from '../assets/unorganized-image.jpg'
+import organizedImage from '../assets/organized-image.jpg'
 
 const Dashboard = () => {
     return (
-        <div>
-            <h2> Welcome to Inventory Management </h2>
+        <div className='home-wrapper'>
+            <div className='image-container'>
+                <img src={unorganizedImage} alt='unorganized'  className='home-image'/>
+                <div className="centered-image-text">Turn This...</div>
+            </div>
+            <div className='image-container'>
+                <img src={organizedImage} alt='organized'  className='home-image'/>
+                <div className="centered-image-text"> Into This! </div>
+            </div>
             
         </div>
     );
